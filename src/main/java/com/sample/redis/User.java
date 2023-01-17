e7433e215453375201cd65cd21d5ad18ad2611c8
Kiran <kiranmc20@gmail.com>
2023-01-17 21:12:25 +0530

    pojo and pom.xml added

---
+++ src/main/java/com/sample/redis/User.java
@@ -0,0 +1,32 @@
+package com.sample.redis;
+
+public class User {
+
+    int id;
+    String name;
+    String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
